8b9f3e0453426106174291ce4c7e5dbdd93a044c
Vaibhav Magar <vaibhavmagar1901@gmail.com>
2022-01-14 13:11:48 +0530

    created react app + navbar + deployed on vercel

---
+++ frontend/src/components/layout/Navbar.js
@@ -0,0 +1,79 @@
+import "./Navbar.css";
+import React, { useState } from "react";
+import { FaSearch, FaUserCircle, FaShoppingCart } from "react-icons/fa";
+import { GiHamburgerMenu } from "react-icons/gi";
+
+const Navbar = () => {
+  const [showMediaIcons, setShowMediaIcons] = useState(false);
+  return (
+    <>
+      <nav className="main-nav">
+        {/* 1st logo part  */}
+        <div className="logo">
+          <h2>
+            <span>S</span>asti
+            <span>C</span>heeze
+          </h2>
+        </div>
+
+        {/* 2nd menu part  */}
+        <div
+          className={
+            showMediaIcons ? "menu-link mobile-menu-link" : "menu-link"
+          }
+        >
+          <ul>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">Products</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+          </ul>
+        </div>
+
+        {/* 3rd social media links */}
+        <div className="nav-tools">
+          <ul className="nav-tools-desktop">
+            <li>
+              <a href="#">
+                <FaSearch className="facebook" />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.instagram.com/thapatechnical/"
+                target="_thapa"
+              >
+                <FaShoppingCart className="instagram" />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.youtube.com/channel/UCwfaAHy4zQUb2APNOGXUCCA"
+                target="_thapa"
+              >
+                <FaUserCircle className="youtube" />
+              </a>
+            </li>
+          </ul>
+
+          {/* hamburget menu start  */}
+          <div className="hamburger-menu">
+            <a href="#" onClick={() => setShowMediaIcons(!showMediaIcons)}>
+              <GiHamburgerMenu />
+            </a>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
